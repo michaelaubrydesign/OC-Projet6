1d2c41440f201d682c5d824985dfd3d1b1ea0047
Michael Aubry <michaelaubrydesign.com>
2023-06-02 13:12:33 +0200

    Step 2.2 - Authentification non fonctionnelle (erreur 405)

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -68,22 +68,50 @@ function filtrerProjetsParCategorie(categoryId, works) {
 
 
 
-/* Step 1.2 - Filtres des travaux :
+
+document.addEventListener('DOMContentLoaded', () => { // Fonction gérant l'authentification ("DOMContentLoaded" sert à exécuter la fonction une fois que la page est chargée entièrement)
+  const loginForm = document.querySelector('#login form'); // Sélectionne le formulaire de connexion
+
+  loginForm.addEventListener('submit', (event) => { // Sélectionne le bouton submit du form
+    event.preventDefault(); // Empeche le rechargement de la page
+
+    // Récupère les valeurs des champs du form
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+    // Créé l'objet body contenant les données à envoyer à l'API
+    const body = {
+      email: email,
+      password: password
+    };
+
+    // Envoi de la requete POST à l'API pour authentification
+    fetch('http://localhost:5678/api/users/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(body)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.token) { // Vérifie si la réponse contient un token
+        localStorage.setItem('token', data.token); // Stock le token dans le local storage
+        window.location.href = 'http://127.0.0.1:5501/'; // Redirige vers l'URL souhaité  *** FAIL ***
+      } else {
+        alert('Identifiant ou mot de passe incorrect');
+      }
+    })
+    .catch(error => {
+      console.error('Erreur lors de l\'authentification', error);
+      alert('Une erreur est survenue lors de l\'authentification');
+    })
+  })
+})
 
 
 
-Mentorat :
-1. Récupérer les boutons de filtre (dans un tableau) - OK
-2. Sur chaque bouton rattacher à un eventListener - OK
-3. Récupérer le data-category-id associé au bouton - OK
-4. Filtrer/Créer un nouveau tableau contenant que les projets correspondants (méthode filter) - OK
-5. Rappeler genererProjet avec le tableau filtré - OK
-
-Penser à modifier la méthode genererProjet pour supprimer le contenu de gallery avant de régénérer le contenu
-
-
-
-Step 2.2
+/*Step 2.2
 
 1. Créer un eventlistener sur le bouton connecter (click)
 2. Récupérer la valeur du champ email (=> variable)
