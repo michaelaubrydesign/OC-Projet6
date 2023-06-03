b1146e1583980bd9366078dcc57f02df69aa68e9
Michael Aubry <michaelaubrydesign.com>
2023-06-03 17:52:26 +0200

    Ajout des boutons Modifier + affichage authenticated

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -1,5 +1,11 @@
 function genererProjets(works) {
   const sectionPortfolio = document.querySelector(".gallery"); // Sélectionne la galerie
+  const boutonsFiltres = document.querySelectorAll(".btn-filtre");
+
+  const token = localStorage.getItem("token"); // Récupère la valeur associé à la clé "token" du localStorage
+  const authenticated = !!token; // La technique "!!" permet de transformer une valeur en booléen
+  
+  
   sectionPortfolio.innerHTML = "";// Vide le contenu HTML
 
 
@@ -16,6 +22,25 @@ function genererProjets(works) {
     projetElement.appendChild(imageProjet);
     projetElement.appendChild(nomProjet);
 
+  }
+
+
+  if (authenticated) {
+    sectionPortfolio.classList.add("authenticated");
+    boutonsFiltres.forEach((bouton) => {
+      bouton.style.display = "none";
+    });
+  } else {
+    sectionPortfolio.classList.remove("authenticated");
+    boutonsFiltres.forEach((bouton) => {
+      bouton.style.display = "block";
+    });
+
+    const modifyButtons = document.querySelectorAll(".btn-modify");
+
+    modifyButtons.forEach((button) => {
+      button.style.display= "none";
+    })
   }
 }
 
@@ -65,81 +90,3 @@ function filtrerProjetsParCategorie(categoryId, works) {
   // Appele la fonction pour générer les projets filtrés
   genererProjets(projetsFiltres);
 }
-
-
-
-
-document.addEventListener('DOMContentLoaded', () => { // Fonction gérant l'authentification ("DOMContentLoaded" sert à exécuter la fonction une fois que la page est chargée entièrement)
-  const loginForm = document.querySelector('#login form'); // Sélectionne le formulaire de connexion
-
-  loginForm.addEventListener('submit', (event) => { // Sélectionne le bouton submit du form
-    event.preventDefault(); // Empeche le rechargement de la page
-
-    // Récupère les valeurs des champs du form
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-
-    // Créé l'objet body contenant les données à envoyer à l'API
-    const body = {
-      email: email,
-      password: password
-    };
-
-    // Envoi de la requete POST à l'API pour authentification
-    fetch('http://localhost:5678/api/users/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(body)
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.token) { // Vérifie si la réponse contient un token
-        localStorage.setItem('token', data.token); // Stock le token dans le local storage
-        window.location.href = 'http://127.0.0.1:5501/'; // Redirige vers l'URL souhaité  *** FAIL ***
-      } else {
-        alert('Identifiant ou mot de passe incorrect');
-      }
-    })
-    .catch(error => {
-      console.error('Erreur lors de l\'authentification', error);
-      alert('Une erreur est survenue lors de l\'authentification');
-    })
-  })
-})
-
-
-
-/*Step 2.2
-
-1. Créer un eventlistener sur le bouton connecter (click)
-2. Récupérer la valeur du champ email (=> variable)
-3. Récupérer valeur mdp (=> variable)
-4. let body = {
-  email:
-  password:
-}
-5. Interroger l'api en répliquant la page fetch (adapter)
-5.5. Gestion des erreurs ( Mdp ou Identifiant invalide)
-6. Extraire result.token
-7. Stocker le token dans le localstorage
-8. Rediriger vers la page d'accueil
-
-9. Quand j'ai un token dans le local storage, je crée les boutons modifier (fonction au chargement de la page)
-
-
-!!!!! Pas de bandeau noir en haut de la maquette
-!!!!! Pas de bouton pour déplacer les projets dans la modale
-
-Step 3.1
-1. Créer eventListener sur les boutons modifier 
-2. Ouvre la modale qui affiche les projets
-3. Bouton supprimer projet + eventListener
-4. Récupérer l'attribut Id du projet
-5. Fetch method Delete
-6. Norme bearer token (autorization bearer token)
-7. Supprimer le projet dans la modale ET dans la galerie du site (tout ça sans refresh) (=> Modifier la variable globale)
-Rappeler genererProjet avec le nouveau tableau (sans le projet dernièrement supprimé)
-
-*/
