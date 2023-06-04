086d75718fafff285b02ba11a5cdc4de7069de04
Michael Aubry <michaelaubrydesign.com>
2023-06-04 17:35:04 +0200

    Step 3.1 Affichage modal - Modal-galery en html prête + bouton delete

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -90,3 +90,35 @@ function filtrerProjetsParCategorie(categoryId, works) {
   // Appele la fonction pour générer les projets filtrés
   genererProjets(projetsFiltres);
 }
+
+
+
+let modal = null                                   // Variable init à null -> Stocker référence de la modal ouverte
+
+const openModal = function (e) {                   // Fonction -> Appelée lorsque l'utilisateur clique sur un élément déclencheur de la modale (param 'e')
+  e.preventDefault()                               // Annule le comportement par défault -> Empêche refresh ou redirect
+  const target = document.querySelector(e.target.getAttribute('href')) // Variable -> Récupère l'élément cible de la modal
+  target.style.display = null                      // Réinitialise la valeur display -> Affiche la modal
+  modal = target
+  modal.addEventListener('click', closeModal)     // Listener click -> appeler fonction closeModal
+  modal.querySelector('.js-modal-close').addEventListener('click', closeModal) // Listener click -> bouton 
+  modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation) // Listener click -> wrapper (stopPropagation)
+}
+
+const closeModal = function (e) {                 // Fonction pour fermer la modal
+  if (modal === null) return                      // Vérifie sur modal est null -> Sinon stop la fonction ici
+  e.preventDefault()
+  modal.style.display = "none"                    // Remet la valeur display: none pour cacher la modal
+  modal.removeEventListener('click', closeModal)  
+  modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
+  modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+  modal = null                                    // Variable modal -> null : pas de modal ouverte
+}
+
+const stopPropagation = function (e) {
+  e.stopPropagation()                             // Empêche la propagation de l'événement
+}
+
+document.querySelectorAll('.js-modal').forEach(btn => {
+  btn.addEventListener('click', openModal)
+})
